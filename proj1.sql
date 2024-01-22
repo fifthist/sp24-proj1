07c97cdf8f51f5913213b8cda24767d2614b8368
fifthist <Overseer.O5.X@gmail.com>
2024-01-22 05:51:45 -0800

    Complete task 1

--- proj1.sql
+++ proj1.sql
@@ -19,31 +19,44 @@ DROP VIEW IF EXISTS q4v;
 -- Question 0
 CREATE VIEW q0(era)
 AS
-  SELECT 1 -- replace this line
+  SELECT MAX(era)
+  FROM pitching
 ;
 
 -- Question 1i
 CREATE VIEW q1i(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT nameFirst, nameLast, birthYear
+  FROM people
+  WHERE weight > 300
 ;
 
 -- Question 1ii
 CREATE VIEW q1ii(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT nameFirst, nameLast, birthYear
+  FROM people
+  WHERE nameFirst LIKE '% %'
+  ORDER BY nameFirst ASC, nameLast ASC
 ;
 
 -- Question 1iii
 CREATE VIEW q1iii(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthYear, AVG(height), COUNT(*)
+  FROM people
+  GROUP BY birthYear
+  ORDER BY birthYear ASC
 ;
 
 -- Question 1iv
 CREATE VIEW q1iv(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthYear, AVG(height), COUNT(*)
+  FROM people
+  GROUP BY birthYear
+  HAVING AVG(height) > 70
+  ORDER BY birthYear ASC
 ;
 
 -- Question 2i
